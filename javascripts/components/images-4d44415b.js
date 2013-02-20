a5425a4761e3610e7f3f0dfb91aa8307405f02b9
Michael LaCroix <michael@lacroixdesign.net>
2013-02-19 21:30:58 -0600

    Automated commit at 2013-02-20 03:30:58 UTC by middleman-deploy 3.0.11

---
+++ javascripts/components/images-4d44415b.js
@@ -0,0 +1,1 @@
+(function(){$.fn.asyncImageLoad=function(){return $(this).each(function(){var e,t;e=$(this),e.hide(),t=e.data("src");if(t)return e.imagesLoaded(e.fadeIn(1e3)).attr("src",t)})},jQuery(function(){return $("[data-behavior='delayedImage']").asyncImageLoad()})}).call(this);
